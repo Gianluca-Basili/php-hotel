a891333a9b892de3106308ed2c214518c701b7db
Gianluca-Basili <gianluc.basili@gmail.com>
2023-07-05 13:36:06 +0200

    esercizio finito

--- index.php
+++ index.php
@@ -47,9 +47,42 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
-    
+    <div class="container mt-4">
+        <div class="row">
+            <div class="col-12">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>nome</th>
+                            <th>desfription</th>
+                            <th>parcheggio</th>
+                            <th>voto</th>
+                            <th>distanza dal centro</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <?php foreach($hotels as $hotel){ ?>
+                        <tr>
+                            <td><?php echo $hotel['name'];?></td>
+                            <td><?php echo $hotel['description'];?></td>
+                            <td><?php echo $hotel['parking']? 'si' : 'no';?></td>
+                            <td><?php echo $hotel['vote'];?>/5</td>
+                            <td><?php echo $hotel['distance_to_center'];?>m</td>
+
+                        </tr>
+                    <?php } ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
+
+
+    
